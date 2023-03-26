6d566f0cda66196b977e2e7f8183cad2a04308e8
skskskskskskskss <128743934+skskskskskskskss@users.noreply.github.com>
2023-03-27 01:10:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,23 +6,16 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
 API_ID = int(getenv("API_ID", "28283744")) #optional
 API_HASH = getenv("API_HASH", "c4e77e41df678ea346e7a146a6718433") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5821324361").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5821324361"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ftvpn:ftvpn@cluster0.4adsj6j.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "6182394661:AAGGJo4kx7h-gZjnFzWon-ON_1MQPnkQHis")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
 PM_LOGGER = getenv("PM_LOGGER", "924550148")
 LOG_GROUP = getenv("LOG_GROUP" ,"924550148")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
